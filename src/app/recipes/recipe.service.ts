31a7a66fcc1f6273b618bbc99cf36b03bfe1a732
henrik <henrik.moelders@gmx.de>
2017-12-22 17:05:59 +0100

    göt veren

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,41 @@
-import { Recipe } from './recipe.model';
+import { EventEmitter, Injectable } from '@angular/core';
 
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is a simple test', 'http://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18180350/051SIP112-grilled-mustard-rosemary-chicken-recipe-alt-main.jpg'),
-        new Recipe('A Test Recipe', 'This is a simple test', 'https://www.campbellsoup.co.uk/img/recipes/6-campbells-vegetarian-pizza-recipe.jpg'),
-        new Recipe('A Test Recipe', 'This is a simple test', 'http://goodtoknow.media.ipcdigital.co.uk/111/000016ef4/543a_orh412w625/Butter-chicken-recipe.jpg')
+        new Recipe(
+        'Tasty Schnitzel', 
+        'A super tasty Schnitzel - Germany fuck yeah!', 
+        'https://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg',
+        [
+            new Ingredient('Meat', 1),
+            new Ingredient('French Fries', 20)
+        ]),
+
+        new Recipe(
+            'Big Fat Awesome Burger', 
+            'What else you need to say?', 
+            'https://dlct0dt1hx57m.cloudfront.net/static/desktop/products/burger-cap.png',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ])
       ];
 
+      constructor(private slService: ShoppingListService) {}
+
       getRecipes() {
-          return this.recipes.slice();
+        return this.recipes.slice();
       }
+
+      addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
+      }
+
 }
